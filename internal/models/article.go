9bb0eaacc4b4a5be40b650f429f0fea04693fd47
Руслан Мурзабеков <101748697+humanityRest0red@users.noreply.github.com>
2025-12-04 01:59:41 +0300

    Update article.go

--- internal/models/article.go
+++ internal/models/article.go
@@ -62,7 +62,7 @@ func RandomArticle() (Article, error) {
 
 func GetArticles() ([]Article, error) {
 absPath, err := filepath.Abs("../../data/test.json")
-	file, err := os.OpenFile(absPath, 0644)
+	file, err := os.OpenFile(absPath, os.O_RDONLY, 0644)
 	if err != nil {
 		return nil, err
 	}
