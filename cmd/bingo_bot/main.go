5ee5a6587cf0b6a83a5cee958a82432bfc91724d
Руслан Мурзабеков <101748697+humanityRest0red@users.noreply.github.com>
2025-12-05 16:43:24 +0300

    Update main.go

--- cmd/bingo_bot/main.go
+++ cmd/bingo_bot/main.go
@@ -20,7 +20,7 @@ func setLogFile() {
         log.Fatalf("Не удалось загрузить часовой пояс: %v", err)
     }
 
- time.Local
+ time.Local = loc
 	file, err := os.OpenFile(filepath.Join("logs", "app.log"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal("Failed to open log file:", err)
